90f66be703ceb1a2e4b6fc740087311faf01f056
Mohamed Alharouni <mohamedharouni1@gmail.com>
2019-10-26 05:09:41 +0200

    Element per thread

--- matrix/main.c
+++ matrix/main.c
@@ -1,3 +1,4 @@
+
 //
 //  main.c
 //  matrix
@@ -12,35 +13,146 @@
 #include <pthread.h>
 #include <strings.h>
 #include <stdbool.h>
-bool readMatrix(FILE * fp, int ** a, int ** b, int * rowA, int * rowB, int * colA, int * colB);
-void* elementByElement(void * args);
+#include <time.h>
 
 
+
+typedef struct {
+	int itC;
+	int itR;
+	int rA;
+	int rB;
+	int cA;
+	int cB;
+}input;
+
+
+
+
+bool readMatrix(void);
+void * elementCalc(void * args);
+void * rowCalc(void * args);
+FILE * fp = NULL ;
+int * ma = NULL;
+int *mb = NULL;
+int * mo = NULL;
+
+int rowA;
+int rowB;
+int colA;
+int colB;
+int iterC=0;
+int iterR=0;
 
 int main(int argc, const char * argv[]) {
 	
-	
-	FILE * fp = NULL ;
-	int * ma = NULL;
-	int *mb = NULL;
-	int rowA;
-	int rowB;
-	int colA;
-	int colB;
-	if (readMatrix(fp, &ma, &mb, &rowA, &rowB, &colA, &colB))
-	{
 
+	int i;
+	int j;
+	int threadCounter=0;
+	int k;
+	clock_t time, time2;
+	pthread_mutex_t lock;
+	
+	
+	
+
+	if (readMatrix())
+	{
+		mo = malloc(rowA * colB * sizeof(int));
+		time = clock();
+		pthread_t t[rowA *colB];
+			for (i=0; i< rowA; i++)
+			{
+				for (j=0; j<colB; j++)
+				{
+					pthread_mutex_lock(&lock);
+					input * input = malloc(sizeof(input));
+					input->cA =colA;
+					input->cB = colB;
+					input-> rA = rowA;
+					input->rB = rowB;
+					input-> itR = iterR;
+					input-> itC = iterC;
+					pthread_mutex_unlock(&lock);
+					pthread_create(&t[threadCounter],NULL,elementCalc, input);
+					iterC++;
+					threadCounter++;
+					
+				}
+				iterC = 0;
+				iterR++;
+			}
+		
+		
+			for(k=0;k<threadCounter; k++)
+			{
+				pthread_join(t[k], NULL);
+			}
+		time= clock() -time;
+		printf("time taken: %f ms \n", (float)time);
+	
+		for (i=0; i<rowA; i++)
+		{
+			for (j=0; j<colB; j++)
+			{
+				printf("%d\t", *(mo + (i * colB ) + j ));
+			}
+			printf("\n");
+		}
+		
+		
+		
+		
+		
+		
+		iterC = 0;
+		iterR = 0;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 	else
 		printf("Error, matrices can not be multiplied\n");
 	
-	return 1;
+	return 0;
 }
 
 
 
-bool readMatrix(FILE * fp, int ** a, int ** b, int * rowA, int * rowB, int * colA, int * colB)
+
+
+
+
+
+
+
+
+
+
+
+
+bool readMatrix()
 {
 
 	int i, j;
@@ -48,37 +160,86 @@ bool readMatrix(FILE * fp, int ** a, int ** b, int * rowA, int * rowB, int * col
 	if ( (fp = fopen("input.txt", "r")) )
 		
 	{
-		fscanf(fp, "%d%d", rowA, colA);
-		*a = malloc( *(colA) * *(rowA) * sizeof( int ));
+		fscanf(fp, "%d%d", &rowA, &colA);
+		ma = malloc( (colA) * (rowA) * sizeof( int ));
 		
-		for (i=0; i<*rowA; i++)
+		for (i=0; i<rowA; i++)
 		{
-			for (j=0; j<*colA; j++)
+			for (j=0; j<colA; j++)
 			{
-				fscanf(fp, "%d", *(a)+(j+*(colA)*i));
+				fscanf(fp, "%d", (ma+ (i * (colA) + j)));
+				//printf("%d\t",*(ma+ (i * (colA) + j)));
 			}
+			printf("\n");
 		}
 		
-		fscanf(fp, "%d%d", rowB, colB);
-		b = malloc( *(colB) * *(rowB) * sizeof( int ));
+		fscanf(fp, "%d%d", &rowB, &colB);
+		mb = malloc( (colB) * (rowB) * sizeof( int ));
 		
-		for (i=0; i<*rowB; i++)
+		for (i=0; i<rowB; i++)
 		{
-			for (j=0; j<*colB; j++)
+			for (j=0; j<colB; j++)
 			{
-				fscanf(fp, "%d", *(b)+(j+*(colB)*i));
+				fscanf(fp, "%d", (mb+ (i * (colB) + j)));
+				//printf("%d\t",*(mb + (i * (colB) + j)));
 			}
+			printf("\n");
 		}
-		
-		if (*rowA == *colB)
+		fclose(fp);
+		if (rowB == colA)
 			return true;
 		else
 			return false;
 	}
-	else
-	return false;
+	return true;
+}
+void* elementCalc(void * args)
+{
+	if (mo)
+	{
+		input * in = (input*) args;
+		int c = in->itC;
+		int r = in->itR;
+		int cb = in->cB;
+		int ca = in -> cA;
+		int i,sum = 0;
+		
+	
+		for(i = 0; i<ca; i++)
+		{
+		//	printf("\n%d\t%d\n", *(ma + ((r * ca) + i)),*(mb + ((cb * i ) + c)));
+			sum+= *(ma + ((r * ca) + i)) * *(mb + ((cb * i ) + c));
+			
+		}
+		*(mo + ( r * ca) + c) = sum;
+		//printf("%d\n",sum);
+	}
+	
+	
+	
+	return NULL;
 }
 
 
 
 
+
+
+void * rowCalc(void * args)
+{
+	if (mo)
+	{
+		input * in = (input*) args;
+		int c = in->itC;
+		int r = in->itR;
+		int cb = in->cB;
+		int ra = in->rA;
+		int ca = in -> cA;
+		int i,j,sum = 0;
+		
+		
+		
+		
+	}
+		return NULL;
+}
